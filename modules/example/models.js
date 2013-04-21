b4277245fcaace55453bf3d4bb0a2fbf59ca2e4a
Brendan McMahon <bernos@gmail.com>
2013-04-21 21:28:02 +1000

    removed unused files

--- modules/example/models.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  return {
-    ModelOne: sequelize.define("ModelOne", {
-      name: DataTypes.STRING,
-      description: DataTypes.TEXT
-    })
-  }
-}
